153846cd4fd9a79ef6f8e87c2ceb4563c0d074f2
Kyle <kyle.fetterhoff@gmail.com>
2020-01-14 11:40:47 -0500

    Add mouse movement logic

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -15,3 +15,39 @@ function draw() {
   context.stroke();
   requestAnimationFrame(draw);
 }
+
+canvas.addEventListener("mousemove", event => {
+  const mousePosition = {
+    x: event.clientX,
+    y: event.clientY
+  };
+
+  const angleDeg =
+    (Math.atan2(
+      mousePosition.y - canvas.height / 2,
+      mousePosition.x - canvas.width / 2
+    ) *
+      180) /
+    Math.PI;
+
+  if (angleDeg >= 0 && angleDeg < 90) {
+    // Mouse in lower right quadrant
+    xVector = 1 - angleDeg / 90;
+    yVector = -(angleDeg / 90);
+  } else if (angleDeg >= 90 && angleDeg <= 180) {
+    // Mouse in lower left quadrant
+    xVector = -(angleDeg - 90) / 90;
+    yVector = -(1 - (angleDeg - 90) / 90);
+  } else if (angleDeg >= -180 && angleDeg < -90) {
+    // Mouse in upper left quadrant
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 + (angleDeg + 90) / 90;
+  } else if (angleDeg < 0 && angleDeg >= -90) {
+    // Mouse in upper right quadrant
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 - (angleDeg + 90) / 90;
+  }
+
+  player.xVector = xVector;
+  player.yVector = yVector;
+});
