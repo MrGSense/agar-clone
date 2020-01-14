237b36e3340be587f88c9035bfb69cc24b79f553
Kyle <kyle.fetterhoff@gmail.com>
2020-01-13 20:22:40 -0500

    Add helmet middleware

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ app.use(express.static(__dirname + "/public"));
 const socketio = require("socket.io");
 const expressServer = app.listen(8080);
 const io = socketio(expressServer);
+const helmet = require("helmet");
+app.use(helmet());
