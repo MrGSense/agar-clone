233f483fad6fcaa438112bdfef962383de083580
Kyle <kyle.fetterhoff@gmail.com>
2020-02-03 14:39:52 -0500

    Add port to server.js for later production

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/public"));
 const socketio = require("socket.io");
-const expressServer = app.listen(8080);
+const PORT = process.env.PORT || 8080;
+const expressServer = app.listen(PORT, () =>
+  console.log(`Server started on port: ${PORT}`)
+);
 const io = socketio(expressServer);
 const helmet = require("helmet");
 app.use(helmet());
